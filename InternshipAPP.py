8d1d70f726d0a2f21936835e2e41d1254aae545a
KelvinSing <143489302+KelvinSing@users.noreply.github.com>
2023-09-24 00:03:52 +0800

    Update InternshipAPP.py

--- InternshipAPP.py
+++ InternshipAPP.py
@@ -502,7 +502,7 @@ def approveCompany(company_name, job_title):
         session['companies'] = updated_companies
 
     # Redirect the user to the "job-list.html" page
-    return render_template('job-list.html')
+    return redirect(url_for('joblist'))
 
 @app.route('/disapprove-company/<company_name>/<job_title>', methods=['GET'])
 def disapproveCompany(company_name, job_title):
