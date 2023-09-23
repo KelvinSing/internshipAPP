0385bdb14f39cb906bac16879f1ccce856cf8c04
KelvinSing <143489302+KelvinSing@users.noreply.github.com>
2023-09-23 16:26:10 +0800

    Update InternshipAPP.py

--- InternshipAPP.py
+++ InternshipAPP.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from pymysql import connections
 import os
 import boto3
